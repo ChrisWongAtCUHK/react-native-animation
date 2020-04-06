c64dc14898054f4bc0f5261050f53c91c087ce9a
Chris Wong <wc38.wong@gmail.com>
2020-04-06 21:01:18 +0800

    Creating a sequence of animations

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './AnimatedParallel';
+import App from './AnimatedSequence';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
