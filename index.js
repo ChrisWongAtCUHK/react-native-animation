de5e0fd909f927491d64f70ce7b929c723e3d2e8
Chris Wong <wc38.wong@gmail.com>
2020-04-06 18:27:58 +0800

    Creating an infinitely spinning loading animation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './AnimatedInput';
+import App from './AnimatedLoop';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
